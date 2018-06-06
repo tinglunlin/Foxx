085118ea74414fe51bca44aceb9cb2d8020b3db4
Ryannnlee <lee810317@hotmail.com.tw>
2018-06-06 16:44:46 +0800

    Add page2

---
+++ src/js/sc/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.qualSlick').slick({
+		centerMode: true,
+		centerPadding: '0px',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: true,
+	});
+	$('.procSlick').slick({
+		centerMode: true,
+		centerPadding: '0px',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: true,
+	});
+});
